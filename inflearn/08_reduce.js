721889522fc58e7323d8bdd9a9995449e7c59705
letminjae <cmjj0824@naver.com>
2022-05-09 00:25:16 +0900

    일곱난쟁이 수정

--- inflearn/08_reduce.js
+++ inflearn/08_reduce.js
@@ -1,12 +1,13 @@
 function solution(arr){
     let answer = arr;
     let _answer = arr.reduce((acc,cur) => acc + cur, 0);
-    for (let i = 0; i < 8; i ++){
-        for (let j = i + 1; i < 9; i++){
-            if(_answer - (answer[i] + answer[j]) === 100){
-                answer.splice(j,1)
-                answer.splice(i,1)
-            }
+    for (let i = 0; i < 8; i++){
+        for (let j = i+1; j < 9; j++){
+            // if(_answer - (answer[i] + answer[j]) === 100){
+            //     answer.splice(j,1)
+            //     answer.splice(i,1)
+            // }
+            console.log(arr[i], arr[j])
         }
     }
     
