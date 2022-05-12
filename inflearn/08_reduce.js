22377ede9bc38e8cd2460e23ac50158d3dd8eadc
letminjae <cmjj0824@naver.com>
2022-05-12 23:25:57 +0900

    A to #

--- inflearn/08_reduce.js
+++ inflearn/08_reduce.js
@@ -3,11 +3,11 @@ function solution(arr){
     let _answer = arr.reduce((acc,cur) => acc + cur, 0);
     for (let i = 0; i < 8; i++){
         for (let j = i+1; j < 9; j++){
-            // if(_answer - (answer[i] + answer[j]) === 100){
-            //     answer.splice(j,1)
-            //     answer.splice(i,1)
-            // }
-            console.log(arr[i], arr[j])
+            if(_answer - (answer[i] + answer[j]) === 100){
+                answer.splice(j,1)
+                answer.splice(i,1)
+            }
+            // console.log(arr[i], arr[j])
         }
     }
     
