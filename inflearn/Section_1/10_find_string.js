c85feb232d026acb98a2d26719cd231fd0ed0bea
letminjae <cmjj0824@naver.com>
2022-05-13 00:23:36 +0900

    10번 다른 풀이 + 섹션 분리

--- inflearn/Section_1/10_find_string.js
+++ inflearn/Section_1/10_find_string.js
@@ -1,10 +1,14 @@
 function solution(s, t){
-    let answer = 0;
-    for(let x of s){
-        if(x === t){
-            answer++;
-        }
-    }
+    // for문을 이용한 풀이
+    // let answer = 0;
+    // for(let x of s){
+    //     if(x === t){
+    //         answer++;
+    //     }
+    // }
+
+    // split을 이용한 풀이
+    let answer = s.split(t).length - 1;
     return answer;
 }
 
