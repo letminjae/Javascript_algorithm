29d98aaea55124590e90c9ad7ce800f23531c685
letminjae <cmjj0824@naver.com>
2022-07-02 11:44:43 +0900

    시저암호, 나머지가 1이되는 수 찾기 풀이 완료

---
+++ Programmars/LV1/06.js
@@ -0,0 +1,16 @@
+// lv1, 시저암호
+
+function solution(s, n) {
+  return s
+    .split("")
+    .map((el) => {
+      if (el == " ") return el;
+      const tmp = el.charCodeAt();
+      return el.toUpperCase().charCodeAt() + n > 90
+        ? String.fromCharCode(tmp + n - 26)
+        : String.fromCharCode(tmp + n);
+    })
+    .join("");
+}
+
+solution("AB", 1);
