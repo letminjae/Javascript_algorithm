50b4bba2fe4f6808ab7bd84d7c8d4b056205efdf
letminjae <cmjj0824@naver.com>
2022-07-12 19:50:11 +0900

    로또 최고순위와 최저순위 풀이 완료

--- Programmars/LV1/23.js
+++ Programmars/LV1/23.js
@@ -1,1 +1,19 @@
+//lv1, 로또의 최고 순위와 최저 순위
 
+function solution(lottos, win_nums) {
+    let answer = [];
+    
+    const correctNumber = lottos.filter((v) => win_nums.includes(v)).length;
+    const zeroCnt = lottos.filter((v) => v === 0).length;
+
+    console.log(correctNumber, zeroCnt);
+
+    let min = 7 - correctNumber >= 6 ? 6 : 7 - correctNumber;
+    let max = min - zeroCnt < 1 ? 1 : min - zeroCnt;
+
+    answer = [max, min]
+
+    return answer;
+}
+
+solution([44, 1, 0, 0, 31, 25],[31, 10, 45, 1, 6, 19])
