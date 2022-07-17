15c145a39e3077c0d7d941f845a968d37c05ea98
letminjae <cmjj0824@naver.com>
2022-07-17 18:52:15 +0900

    LV2 기능 개발 풀이 완료

---
+++ Programmars/LV2/03.js
@@ -0,0 +1,27 @@
+//lv2, 기능개발
+
+function solution(progresses, speeds) {
+  let answer = [];
+  let days = 1;
+  let cnt = 0;
+  let progress = 0;
+
+  while (progresses[0]) {
+    progress = progresses[0] + (speeds[0] * days);
+    if (progress >= 100) {
+      cnt++;
+      progresses.shift();
+      speeds.shift();
+    } else {
+      if (cnt > 0) {
+        answer.push(cnt);
+      }
+      days++;
+      cnt = 0;
+    }
+  }
+  answer.push(cnt);
+  return answer;
+}
+
+solution([93, 30, 55], [1, 30, 5]);
